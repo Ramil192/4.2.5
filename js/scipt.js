0b07d6f0a2a3aa4e4bd4fb3b4ffabcda526fe484
Ramil192 <61628421+Ramil192@users.noreply.github.com>
2020-06-10 11:12:14 +0300

    2

--- js/scipt.js
+++ js/scipt.js
@@ -1,88 +1,82 @@
-const serch = document.querySelector('.serchBody__input');
-const addRepositories = document.querySelector('#addRepositories');
-const selectedRepositories = document.querySelector('#selectedRepositories');
-let count = 0;
-const debounce = (fn, debounceTime) => {
-	let timoute;
-	return function () {
-		const fnCall = () => { fn.apply(this, arguments) }
-		clearTimeout(timoute);
-		timoute = setTimeout(fnCall, debounceTime)
-	}
-}
-
-function createRepositories(repositorierData) {
-	const repositorieElement = createElement('div', 'addRepositories__repositorier');
-	repositorieElement.addEventListener('click', () => showRepositorierData(repositorierData));
-	repositorieElement.innerHTML = `<span class="addRepositories__repositorier_name">${repositorierData.name}</span>`;
-	addRepositories.append(repositorieElement);
-}
-
-function showRepositorierData(repositorierData) {
-	serch.value = '';
-	cleaerRepositores()
-	if (count != 3) {
-		count++;
-		const repositorieElement = createElement('div', 'selectedRepositories__repositorier');
-		const repositorieWrapper = createElement('div', 'selectedRepositories__wrapper');
-		repositorieElement.innerHTML = `<span class="selectedRepositories__repositorier_name">Name: ${repositorierData.name}</span>
-												  <span class="selectedRepositories__repositorier_owner">Owner:${repositorierData.owner['login']}</span>
-												  <span class="selectedRepositories__repositorier_starts">Starts:${repositorierData.stargazers_count}</span>`;
-	
-		const repositorieElementClose = createElement('div', 'selectedRepositories__close');
-		
-		repositorieWrapper.append(repositorieElement);
-		repositorieWrapper.append(repositorieElementClose);
-		repositorieElementClose.addEventListener('click', () => deleteRepositori(repositorieWrapper));
-		selectedRepositories.append(repositorieWrapper);
+class View {
+	constructor() {
+		this.serch = document.querySelector('.serchBody__input');
+		this.addRepositories = document.querySelector('#addRepositories');
+		this.selectedRepositories = document.querySelector('#selectedRepositories');
+		this.maximumSelectedRepository = 0;
 	}
 
-}
-function deleteRepositori(repozitory) {
-	repozitory.remove();
-	count--;
-}
-function createElement(elementTag, elementClass) {
-	const element = document.createElement(elementTag);
-	if (elementClass) {
-		element.classList.add(elementClass);
-	}
-	return element;
-}
+	showRepositorierData(repositorierData) {
+		this.serch.value = '';
+		this.cleaerRepositores();
+		if (this.maximumSelectedRepository != 3) {
+			this.maximumSelectedRepository++;
+			this.repositorieElement = this.createElement('div', 'selectedRepositories__repositorier');
+			this.repositorieWrapper = this.createElement('div', 'selectedRepositories__wrapper');
+			this.repositorieElement.innerHTML = `<span class="selectedRepositories__repositorier_name">Name: ${repositorierData.name}</span>
+													       <span class="selectedRepositories__repositorier_owner">Owner:${repositorierData.owner['login']}</span>
+													       <span class="selectedRepositories__repositorier_starts">Starts:${repositorierData.stargazers_maximumSelectedRepository}</span>`;
 
-function cleaerRepositores() {
-	addRepositories.innerHTML = '';
+			this.repositorieElementClose = this.createElement('div', 'selectedRepositories__close');
+
+			this.repositorieWrapper.append(this.repositorieElement);
+			this.repositorieWrapper.append(this.repositorieElementClose);
+			this.repositorieElementClose.addEventListener('click', () => this.deleteRepositori(this.repositorieWrapper));
+			this.selectedRepositories.append(this.repositorieWrapper);
+		}
+	}
+	createElement(elementTag, elementClass) {
+		const element = document.createElement(elementTag);
+		if (elementClass) {
+			element.classList.add(elementClass);
+		}
+		return element;
+	}
+	createRepositories(repositorierData) {
+		this.repositorieElement = this.createElement('div', 'addRepositories__repositorier');
+		this.repositorieElement.addEventListener('click', () => this.showRepositorierData(repositorierData));
+		this.repositorieElement.innerHTML = `<span class="addRepositories__repositorier_name">${repositorierData.name}</span>`;
+		this.addRepositories.append(this.repositorieElement);
+	}
+	deleteRepositori(repozitory) {
+		repozitory.remove();
+		this.maximumSelectedRepository--;
+	}
+	debounce(fn, debounceTime) {
+		let timoute;
+		return function () {
+			const fnCall = () => { fn.apply(this, arguments) }
+			clearTimeout(timoute);
+			timoute = setTimeout(fnCall, debounceTime)
+		}
+	}
+	cleaerRepositores() {
+		addRepositories.innerHTML = '';
+	}
 }
 
 class Serch {
 	constructor(view) {
 		this.view = view;
-		this.view.addEventListener('keyup', debounce(this.serchRepositories.bind(this), 500))
+		this.view.serch.addEventListener('keyup', this.view.debounce(this.serchRepositories.bind(this), 500))
 	}
 	async serchRepositories() {
-		const serchValu = serch.value;
+		const serchValu = this.view.serch.value;
 		if (serchValu) {
-
-			return await fetch(`https://api.github.com/search/repositories?q=${this.view.value}&per_page=5`)
+			return await fetch(`https://api.github.com/search/repositories?q=${this.view.serch.value}&per_page=5`)
 				.then(res => {
 					if (res.ok) {
 						res.json().then(res => {
-							cleaerRepositores();
-							res.items.forEach(repositorier => createRepositories(repositorier));
-
+							this.view.cleaerRepositores();
+							res.items.forEach(repositorier => this.view.createRepositories(repositorier));
 						})
-					} else {
-						//..........
 					}
 				})
 		} else {
-			cleaerRepositores();
+			this.view.cleaerRepositores();
 		}
 	}
 }
-
-
-
-new Serch(serch);
+new Serch(new View);
 
 
